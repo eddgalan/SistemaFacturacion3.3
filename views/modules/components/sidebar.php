b25675bc3b42bbb736fc87f059a182cf775d4a24
‘Edson <edd.galan@hotmail.com>
2021-07-06 18:18:11 -0500

    Paginación del menú | #30

--- views/modules/components/sidebar.php
+++ views/modules/components/sidebar.php
@@ -40,35 +40,35 @@
                   <!-- ..:: Administrar ::.. -->
                   <?php
                     if( $panel_admin ){
-                      echo "<div class='seccion list-group-item list-group-item-action bg-light'><span> <i class='fas fa-user-cog'></i> Administrar</span></div>\n";
-                      $panel = "<div class='panel'>\n";
+                      echo "<div class='seccion list-group-item list-group-item-action bg-light'><span> <i class='fas fa-user-cog'></i> Administrar</span></div>\n\t\t\t\t\t";
+                      $panel = "<div class='panel'>\n\t\t\t\t\t\t";
                       // Usuarios
                       if( $permisos['Admin_usuario'] != 0 ){
-                        $panel .= "<a class='nav-link' href='". $data['host'] ."/administrar/usuarios'> <i class='fas fa-user'></i> Usuarios</a>\n";
+                        $panel .= "<a class='nav-link' href='". $data['host'] ."/administrar/usuarios'> <i class='fas fa-user'></i> Usuarios</a>\n\t\t\t\t\t\t";
                       }
                       // Grupos
                       if( $permisos['Admin_grupos'] != 0 ){
-                        $panel .= "<a class='nav-link' href='". $data['host'] ."/administrar/grupos'> <i class='fas fa-users'></i> Grupos</a>\n";
+                        $panel .= "<a class='nav-link' href='". $data['host'] ."/administrar/grupos'> <i class='fas fa-users'></i> Grupos</a>\n\t\t\t\t\t\t";
                       }
                       // Perfiles
                       if( $permisos['Admin_perfiles'] != 0 ){
-                        $panel .= "<a class='nav-link' href='". $data['host'] ."/administrar/perfiles'> <i class='fas fa-user-tie'></i> Perfiles</a>\n";
+                        $panel .= "<a class='nav-link' href='". $data['host'] ."/administrar/perfiles'> <i class='fas fa-user-tie'></i> Perfiles</a>\n\t\t\t\t\t\t";
                       }
                       // Emisores
                       if( $permisos['Admin_emisores'] != 0 ){
-                        $panel .= "<a class='nav-link' href='". $data['host'] ."/administrar/emisores'> <i class='fas fa-building'></i> Emisores</a>\n";
+                        $panel .= "<a class='nav-link' href='". $data['host'] ."/administrar/emisores'> <i class='fas fa-building'></i> Emisores</a>\n\t\t\t\t\t\t";
                       }
                       // Clientes
                       if( $permisos['Admin_clientes'] != 0 ){
-                        $panel .= "<a class='nav-link' href='". $data['host'] ."/administrar/clientes'> <i class='far fa-address-book'></i> Clientes</a>\n";
+                        $panel .= "<a class='nav-link' href='". $data['host'] ."/administrar/clientes'> <i class='far fa-address-book'></i> Clientes</a>\n\t\t\t\t\t\t";
                       }
                       // Productos y Servicios
                       if( $permisos['Admin_prodserv'] != 0 ){
-                        $panel .= "<a class='nav-link' href='". $data['host'] ."/administrar/prodserv'> <i class='fas fa-box'></i> Productos/Servicios</a>\n";
+                        $panel .= "<a class='nav-link' href='". $data['host'] ."/administrar/prodserv'> <i class='fas fa-box'></i> Productos/Servicios</a>\n\t\t\t\t\t\t";
                       }
                       // Series
                       if( $permisos['Admin_series'] != 0 ){
-                        $panel .= "<a class='nav-link' href='". $data['host'] ."/catalogosSAT/series'> <i class='fas fa-font'></i> Series</a>\n";
+                        $panel .= "<a class='nav-link' href='". $data['host'] ."/catalogosSAT/series'> <i class='fas fa-font'></i> Series</a>\n\t\t\t\t\t";
                       }
                       $panel .= "</div>\n";
                       echo $panel;
@@ -77,10 +77,10 @@
                   <!-- ..:: Comprobantes ::.. -->
                   <?php
                     if( $panel_comprobantes ){
-                      echo "<div class='seccion list-group-item list-group-item-action bg-light'><span> <i class='fas fa-money-check-alt'></i> Comprobantes</span></div>\n";
-                      $panel = "<div class='panel'>\n";
+                      echo "<div class='seccion list-group-item list-group-item-action bg-light'><span> <i class='fas fa-money-check-alt'></i> Comprobantes</span></div>\n\t\t\t\t\t";
+                      $panel = "<div class='panel'>\n\t\t\t\t\t\t";
                       if( $permisos['Comprobantes_facturas'] ){
-                        $panel .= "<a class='nav-link' href='". $data['host'] ."/CFDIs/facturas'> <i class='fas fa-file-invoice-dollar'></i> Facturas</a>\n";
+                        $panel .= "<a class='nav-link' href='". $data['host'] ."/CFDIs/facturas'> <i class='fas fa-file-invoice-dollar'></i> Facturas</a>\n\n\t\t\t\t\t";
                       }
                       $panel .= "</div>\n";
                       echo $panel;
@@ -94,10 +94,10 @@
                   <!-- ..:: Reportes ::.. -->
                   <?php
                     if( $panel_reportes ){
-                      echo "<div class='seccion list-group-item list-group-item-action bg-light'><span> <i class='fas fa-money-check-alt'></i> Reportes</span></div>\n";
-                      $panel = "<div class='panel'>\n";
+                      echo "<div class='seccion list-group-item list-group-item-action bg-light'><span> <i class='fas fa-money-check-alt'></i> Reportes</span></div>\n\t\t\t\t\t";
+                      $panel = "<div class='panel'>\n\t\t\t\t\t\t";
                       if( $permisos['Reportes_reportemensual'] ){
-                        $panel .= "<a class='nav-link' href='". $data['host'] ."/reportes/mensual'> <i class='far fa-calendar-alt'></i> Reporte Mensual</a>\n";
+                        $panel .= "<a class='nav-link' href='". $data['host'] ."/reportes/mensual'> <i class='far fa-calendar-alt'></i> Reporte Mensual</a>\n\t\t\t\t\t";
                       }
                       $panel .= "</div>\n";
                       echo $panel;
@@ -106,27 +106,27 @@
                   <!-- ..:: Catálogos SAT ::.. -->
                   <?php
                     if( $panel_catsat ){
-                      echo "<div class='seccion list-group-item list-group-item-action bg-light'><span> <i class='fas fa-funnel-dollar'></i> Catálogos SAT</span></div>\n";
-                      $panel = "<div class='panel'>\n";
+                      echo "<div class='seccion list-group-item list-group-item-action bg-light'><span> <i class='fas fa-funnel-dollar'></i> Catálogos SAT</span></div>\n\t\t\t\t\t";
+                      $panel = "<div class='panel'>\n\t\t\t\t\t\t";
                       // Claves de productos y servicios
                       if( $permisos['CatSAT_claves_prodserv'] ){
-                        $panel .= "<a class='nav-link' href='". $data['host'] ."/catalogosSAT/prod_serv'> <i class='fas fa-apple-alt'></i> Claves ProdServ</a>\n";
+                        $panel .= "<a class='nav-link' href='". $data['host'] ."/catalogosSAT/prod_serv'> <i class='fas fa-apple-alt'></i> Claves ProdServ</a>\n\t\t\t\t\t\t";
                       }
                       // Unidades
                       if( $permisos['CatSAT_unidades'] ){
-                        $panel .= "<a class='nav-link' href='". $data['host'] ."/catalogosSAT/unidades'> <i class='fas fa-weight'></i> Unidades</a>\n";
+                        $panel .= "<a class='nav-link' href='". $data['host'] ."/catalogosSAT/unidades'> <i class='fas fa-weight'></i> Unidades</a>\n\t\t\t\t\t\t";
                       }
                       // Fromas de Pago
                       if( $permisos['CatSAT_formaspago'] ){
-                        $panel .= "<a class='nav-link' href='". $data['host'] ."/catalogosSAT/formas_pago'> <i class='fas fa-hand-holding-usd'></i> Formas de Pago</a>\n";
+                        $panel .= "<a class='nav-link' href='". $data['host'] ."/catalogosSAT/formas_pago'> <i class='fas fa-hand-holding-usd'></i> Formas de Pago</a>\n\t\t\t\t\t\t";
                       }
                       // Monedas
                       if( $permisos['CatSAT_monedas'] ){
-                        $panel .= "<a class='nav-link' href='". $data['host'] ."/catalogosSAT/monedas'> <i class='fas fa-coins'></i> Monedas</a>\n";
+                        $panel .= "<a class='nav-link' href='". $data['host'] ."/catalogosSAT/monedas'> <i class='fas fa-coins'></i> Monedas</a>\n\t\t\t\t\t\t";
                       }
                       // Impuestos
                       if( $permisos['CatSAT_impuestos'] ){
-                        $panel .= "<a class='nav-link' href='". $data['host'] ."/catalogosSAT/impuestos'> <i class='fas fa-dollar-sign'></i> Impuestos</a>\n";
+                        $panel .= "<a class='nav-link' href='". $data['host'] ."/catalogosSAT/impuestos'> <i class='fas fa-dollar-sign'></i> Impuestos</a>\n\t\t\t\t\t";
                       }
                       $panel .= "</div>\n";
                       echo $panel;
