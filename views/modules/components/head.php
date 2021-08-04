8474670b9aa82169c0b16d5dbf649e4f45e84795
‘Edson <edd.galan@hotmail.com>
2021-08-04 03:10:12 -0500

    Botones responsives

--- views/modules/components/head.php
+++ views/modules/components/head.php
@@ -4,6 +4,7 @@
     <link rel="stylesheet" href="<?= $data['host']?>/views/assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= $data['host']?>/views/assets/css/simple-sidebar.css">
     <link rel="stylesheet" href="<?= $data['host']?>/views/assets/css/styles.css">
+    <link rel="stylesheet" href="<?= $data['host']?>/views/assets/css/responsive.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
